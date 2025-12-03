69a349f6b467266d6610f417f340e14b1d79641d
tquintard <95873239+tquintard@users.noreply.github.com>
2025-12-03 16:30:44 +0000

    test request url

--- 🏠_Homepage.py
+++ 🏠_Homepage.py
@@ -1,11 +1,13 @@
 import streamlit as st
 from PIL import Image
 from core.app_manager import set_page_settings
+import requests
+
 
 
 # Internet Browser tab name
 set_page_settings(title="Advent of Code Solver", icon="🎄")
-
+test()
 # Homepage section
 # Display an image on the homepage
 img = Image.open("resources/pictures/aoc24.png")
@@ -15,3 +17,11 @@ st.write(
     "You can drop your inputs and run my code to check whether your solution is correct.")
 st.write("Select a puzzle from the menu to begin.")
 
+
+def test():
+    url = "https://adventofcode.com/"
+    response = requests.get(url)
+    if response.status_code == 200:
+        st.write(response.text)
+    else:
+        st.error(f"{e}")
