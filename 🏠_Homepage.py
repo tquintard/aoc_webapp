af140ca940e9c7bb5ed4aa37cc39c4968e9480ac
tquintard <95873239+tquintard@users.noreply.github.com>
2025-12-03 16:40:57 +0000

    Remove test function for fetching Advent of Code input
    Removed the test function that fetched data from a URL.

--- 🏠_Homepage.py
+++ 🏠_Homepage.py
@@ -3,17 +3,8 @@ from PIL import Image
 from core.app_manager import set_page_settings
 import requests
 
-def test():
-    url = "https://adventofcode.com/2025/day/1/input"
-    response = requests.get(url)
-    if response.status_code == 200:
-        st.write(response.text)
-    else:
-        st.error("error")
-
 # Internet Browser tab name
 set_page_settings(title="Advent of Code Solver", icon="🎄")
-test()
 # Homepage section
 # Display an image on the homepage
 img = Image.open("resources/pictures/aoc24.png")
