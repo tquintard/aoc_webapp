feb78fb562101a7a5ccfbcc56fd8d6bc0507bad4
tquintard <95873239+tquintard@users.noreply.github.com>
2025-12-03 10:45:11 +0000

    Delete pages/99_📚Common_Modules.py

--- pages/99_📚Common_Modules.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("📚Common Modules")
-st.write("Find here the common modules used in the puzzles.")
-# Display the content of the corresponding Python module
-with open(f"modules/common/__init__.py", "r") as f:
-    code = f.read()
-st.code(code, language="python")
