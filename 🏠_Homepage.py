ae3ef1d51756d03429e9b4a44799a68259295b6d
tquintard <95873239+tquintard@users.noreply.github.com>
2025-12-05 16:21:50 +0000

    Add files via upload

--- 🏠_Homepage.py
+++ 🏠_Homepage.py
@@ -1,17 +1,17 @@
 import streamlit as st
 from PIL import Image
 from core.app_manager import set_page_settings
-import requests
+
 
 # Internet Browser tab name
 set_page_settings(title="Advent of Code Solver", icon="🎄")
+
 # Homepage section
 # Display an image on the homepage
-img = Image.open("resources/pictures/aoc24.png")
+img = Image.open("assets/pictures/aoc24.png")
 st.image(img, width = 800)
 st.write("This web app aims to share my Python code I used to solve AoC puzzles.")
 st.write(
     "You can drop your inputs and run my code to check whether your solution is correct.")
 st.write("Select a puzzle from the menu to begin.")
-
 
