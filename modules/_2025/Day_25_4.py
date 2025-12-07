4316c0260941b905a8c66d30cee380c417e1f4d7
tquintard <95873239+tquintard@users.noreply.github.com>
2025-12-07 16:28:23 +0000

    Add files via upload

---
+++ modules/_2025/Day_25_4.py
@@ -0,0 +1,20 @@
+def main(inputs: str) -> tuple[int, int]:
+    # Accumulators for each configured maximum power.
+
+    return 0,0
+
+
+
+
+SAMPLE = """\
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.\
+"""
